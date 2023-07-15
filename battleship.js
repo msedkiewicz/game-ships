ae5f145ff6eeed0ee8490f2a0d6b85a612b79eeb
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-07-15 21:01:20 +0200

    Add loop for cell number checking

--- battleship.js
+++ battleship.js
@@ -9,3 +9,20 @@ let hits = 0;
 let guesses = 0;
 
 let isSunk = false;
+
+/* checking if ship was sunk or if game is still on */
+
+while (isSunk === false) {
+    guess = prompt("Gotów? Cel, pal! (Podaj liczbę z zakresu od 0 do 6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Proszę podać prawidłowy numer (od 0 do 6)!");
+    } else {
+        guesses = guesses + 1;
+
+        /* checking if ship was hit */
+        if ( guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+        }
+    }
+}
