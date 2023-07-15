027fd2589e4ba3dfc54f9cdef5bd24b10236103b
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2023-07-15 20:55:26 +0200

    Add starting variables

---
+++ battleship.js
@@ -0,0 +1,11 @@
+/* Ship localization variables */
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+/* Game variables */
+let guess;
+let hits = 0;
+let guesses = 0;
+
+let isSunk = false;
